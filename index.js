917440aeb4b29c5f3d8b268b2b37da244ef740b9
Jana Khanji <jana.khanji@ieee.org>
2020-12-08 10:47:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+function findMatching (arr, name) {
+  return arr.filter(function (item) {
+    return item.toLowerCase() === name.toLowerCase();
+  });
+}
+
+function fuzzyMatch (list, provided) {
+  return list.filter(function (name) {
+    return name.slice(0, provided.length) === provided;
+  });
+}
+
+function matchName (drivers ,name){
+  return drivers.filter(function (item) {
+    return item.name.toLowerCase() === name.toLowerCase();
+  });
+  
+}
