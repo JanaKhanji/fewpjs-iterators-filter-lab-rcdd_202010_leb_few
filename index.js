0ad4441292335770f521dc4b3cca5572385058d3
Jana Khanji <jana.khanji@ieee.org>
2020-12-08 10:42:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ function findMatching (arr, name) {
   });
 }
 
+function fuzzyMatch (list, partialName) {
+  let lengthOfName = partialName.length;
+  return list.filter(function (driverName) {
+    return driverName.slice(0, lengthOfName) === partialName;
+  });
+}
